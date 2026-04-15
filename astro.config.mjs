b992c4344acd77d127dab0bea02e419bb580777b
kobeyvines <12995.2020@students.ku.ac.ke>
2026-04-15 18:39:58 +0300

    fix: routing and base url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   // GitHub Pages routing configuration
-  site: 'https://Postcards-from-Nyumbani.github.io',
-  base: '/postcards-from-nyumbani',
+// The absolute domain (dashes)
+  site: 'https://postcards-from-nyumbani.github.io',
+  
+  // The exact repository name (underscores)
+  base: '/postcards_from_nyumbani',
 
   integrations: [
     tailwind({
